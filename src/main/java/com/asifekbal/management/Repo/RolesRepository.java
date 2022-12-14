889e2c9ee55ab3400cd709d2b0b506290fec23c9
Ekbal41 <mdasifekbalshagor@gmail.com>
2022-12-15 01:24:13 +0600

    auth added

---
+++ src/main/java/com/asifekbal/management/Repo/RolesRepository.java
@@ -0,0 +1,11 @@
+package com.asifekbal.management.Repo;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.asifekbal.management.Shema.Role;
+
+public interface RolesRepository extends JpaRepository<Role,Integer> {
+    
+}
